4aae650fc9d468b631ef5a60fde272bd634f1ed2
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-09 18:40:18 +0200

    vyřešení první části úkolu - script funguje pouze pro první hru

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4' // Tento řádek 
 // Kód níže je pouze ukázka použití funkce findWinner. Následující řádky smažte.
 
 // Ukázka použití pro rozehranou hru 3x3, kde začínající hráč kolečko hrál všechny své tři tady do prvního sloupečku a druhý hráč křížek všechny své dva tahy do druhého sloupečku.
-const ukazkoveHerniPole = ['o', 'x', '_', 'o', 'x', '_', 'o', '_', '_']
+// const ukazkoveHerniPole = ['o', 'x', '_', 'o', 'x', '_', 'o', '_', '_']
 // Pro lepší představu stejné pole lze zapsat i takto:
 /*
 const ukazkoveHerniPole = [
@@ -12,5 +12,25 @@ const ukazkoveHerniPole = [
 	'o', '_', '_',
 ]
 */
-const vitez = findWinner(ukazkoveHerniPole)
-console.log(`Vyhrál hráč se symbolem ${vitez}.`) // 'Vyhrál hráč se symbolem o.'
+
+const hra = document.querySelector('.hra');
+const policka = hra.querySelectorAll('.policko');
+const vysledek = hra.querySelector('.vysledek');
+// console.log(policka)
+
+const nove = Array.from(policka).map((pole) => {
+	if (pole.classList.contains("kolecko")) {
+		return "o"
+	} else if (pole.classList.contains("krizek")) {
+		return "x"
+	}
+	return "_"
+})
+
+// onsole.log(nove)
+
+const vitez1 = findWinner(nove)
+vysledek.textContent = `Vyhrál hráč se symbolem ${vitez1}.`
+
+// const vitez = findWinner(ukazkoveHerniPole)
+// console.log(`Vyhrál hráč se symbolem ${vitez}.`) // 'Vyhrál hráč se symbolem o.'
