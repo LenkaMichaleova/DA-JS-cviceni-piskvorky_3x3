1980006204b4423fd63fe115a65b475c13616537
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-09 19:34:26 +0200

    vyřešená druhá část úkolu včetně bonusu

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
-import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4' // Tento řádek vám umožňuje používat funkci findWinner od jiného programátora. Více se o zápisu s import dozvíte v pozdějších lekcích.
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
 
-// Kód níže je pouze ukázka použití funkce findWinner. Následující řádky smažte.
+const hry = document.querySelectorAll('.hra');
 
-// Ukázka použití pro rozehranou hru 3x3, kde začínající hráč kolečko hrál všechny své tři tady do prvního sloupečku a druhý hráč křížek všechny své dva tahy do druhého sloupečku.
-// const ukazkoveHerniPole = ['o', 'x', '_', 'o', 'x', '_', 'o', '_', '_']
-// Pro lepší představu stejné pole lze zapsat i takto:
-/*
-const ukazkoveHerniPole = [
-	'o', 'x', '_',
-	'o', 'x', '_',
-	'o', '_', '_',
-]
-*/
+hry.forEach((hra) => {
+	const policka = Array.from(hra.querySelectorAll(".policko")).map((pole) => {
+		if (pole.classList.contains("kolecko")) {
+			return "o"
+		} else if (pole.classList.contains("krizek")) {
+			return "x"
+		}
+		return "_"
+	})
+	const vysledek = hra.querySelector('.vysledek')
+	const vitez = findWinner(policka)
+	//console.log(vysledek)
+	//console.log(vitez)
 
-const hra = document.querySelector('.hra');
-const policka = hra.querySelectorAll('.policko');
-const vysledek = hra.querySelector('.vysledek');
-// console.log(policka)
-
-const nove = Array.from(policka).map((pole) => {
-	if (pole.classList.contains("kolecko")) {
-		return "o"
-	} else if (pole.classList.contains("krizek")) {
-		return "x"
+	if (vitez === "o" || vitez === "x") {
+		vysledek.textContent = `Vyhrál hráč se symbolem ${vitez}.`
+	} else if (vitez === "tie"){
+		vysledek.textContent = `Hra skončila remízou.`
+	} else {
+		vysledek.textContent = `Hra ještě probíhá.`
 	}
-	return "_"
 })
-
-// onsole.log(nove)
-
-const vitez1 = findWinner(nove)
-vysledek.textContent = `Vyhrál hráč se symbolem ${vitez1}.`
-
-// const vitez = findWinner(ukazkoveHerniPole)
-// console.log(`Vyhrál hráč se symbolem ${vitez}.`) // 'Vyhrál hráč se symbolem o.'
